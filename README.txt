17928026fa2e6a80afd5ece43657fec0c64ea9c3
Aaiz Ahmed <aaiza2@umbc.edu>
2017-01-01 14:45:57 -0500

    files added

---
+++ README.txt
@@ -0,0 +1,23 @@
+Aaiz N Ahmed 
+
+This directory contains some generic Java list data structures like LinkedList, ArrayList and Stack.
+All the list data structures were built from scratch. These structures can be used to store anytype of data.
+
+References Used:
+1) Data Structures and Algorithm Analysis in Java by Mark Allen Weiss. 
+2) Ooracle Java APIs
+
+Language Used: Java
+Compiler/Running environment: Java Development Kit (JDK) is required on the machine in which you
+	want to run the program. Java Runtime Environment (JRE) should be fine to run the program as
+	well.
+
+Download Java SDK for your architecture using the following URL: 
+http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+
+
+Comments: 
+
+Contact:  aaiznahmed@gmail.com or aaiza2@umbc.edu
+LinkedIn: https://www.linkedin.com/in/aaizahmed
+GitHub:   https://github.com/AaizAhmed
---
+++ StackUsingLinkedList.java
@@ -0,0 +1,169 @@
+package lists;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class StackUsingLinkedList<T> implements Iterable<T> {
+	
+	private Node<T> front;
+	private int size, modCount;
+	
+	/**
+	 * Constructor:
+	 */
+	public StackUsingLinkedList () 
+	{	doClear();	}
+	
+	/**
+	 * Node class
+	 */
+	private class Node<T> {
+		
+		private Node<T> next;
+		private T data;
+		
+		public Node (T data, Node<T> next) {
+			
+			this.data = data;
+			this.next = next;
+		}	
+	}
+	
+	public int getSize() 
+	{	return size;	}
+	
+	public boolean isEmpty()
+	{	return size == 0;	}
+	
+	public void clear()
+	{	doClear();	}
+	
+	private void doClear () {
+		
+		front = new Node<T> (null, null);		
+		size = 0;
+		modCount++;
+	}
+	
+	public boolean push (T data) {
+		
+		pushElement(data);
+		return true;
+	}
+	
+	private void pushElement (T data) {
+		
+		Node<T> newNode = new Node<T> (data, front.next);
+		front.next = newNode;
+		
+		size++;
+		modCount++;
+	}
+	
+	public T pop(){
+		
+		return popElement();
+	}
+	
+	private T popElement() {
+		
+		if (isEmpty())
+		{	throw new NoSuchElementException( );	}
+		
+		Node<T> removedNode = front.next;		
+		front.next = removedNode.next;		
+		size--;
+		modCount++;
+		
+		return removedNode.data;
+	}
+	
+	public T peek() 
+	{	return getTop().data;	}
+	
+	private Node<T> getTop() {
+		
+		if (isEmpty())
+		{	throw new NoSuchElementException( );	}
+		
+		Node<T> topNode = front.next;
+		
+		return topNode;
+	}
+	
+	public String toString() {
+		
+		String str = "[";
+		
+		StackIterator itr = new StackIterator();
+						
+		while(itr.hasNext()) {	
+			
+			str += itr.next().toString();
+			
+			if (itr.hasNext())
+			{	str += ", ";	}
+		}
+		
+		str += "]";
+		
+		return str;
+	}
+	
+	public Iterator<T> iterator() 
+	{	return new StackIterator();   }
+	
+	private class StackIterator implements Iterator<T> {
+
+		private Node<T> current = front.next;
+		private int expectedModCount = modCount;
+		private boolean okToRemove = false;
+	
+		public boolean hasNext() 
+		{	return current != null;	}
+
+		public T next() {
+	
+			if (expectedModCount != modCount)
+			{	throw new java.util.ConcurrentModificationException( );	}
+			
+			if (!hasNext())
+			{	throw new NoSuchElementException();	}
+			
+			T nextItem = current.data;
+			current = current.next;
+			okToRemove = true;
+			
+			return nextItem;
+		}
+		
+		public void remove() {
+			
+			if (expectedModCount != modCount) 
+			{	throw new java.util.ConcurrentModificationException( );	}
+				
+			if( !hasNext( ) )
+			{	throw new java.util.NoSuchElementException( );	}
+				
+			if (okToRemove)
+			{	StackUsingLinkedList.this.popElement();   }
+			
+			expectedModCount++;
+			okToRemove = false;			
+		}		
+	}
+
+	public static void main (String [] args) {
+		
+		StackUsingLinkedList<Integer> stack = new StackUsingLinkedList<Integer>();
+		
+		stack.push(41);
+		stack.push(42);
+		stack.push(43);
+		stack.push(44);
+		stack.push(45);
+		
+		System.out.println (stack);
+	}
+	
+}
